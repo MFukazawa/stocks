d412df34bb51de4a27b046ecfe5b0d8b893b87ee
Micah Fukazawa <micahfukazawa@gmail.com>
2020-03-23 17:05:47 +0900

    Update dropdown menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,28 +2,50 @@
   <div class="container">
     <nav class="mt-4 navbar navbar-expand-lg navbar-light bg-light">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <router-link to="/" class="navbar-brand nav-link"
-            >Stock Trader</router-link
-          >
-        </li>
-        <li class="nav-item">
-          <router-link to="" class="nav-link">Portfolio</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/stocks" class="nav-link">Stocks</router-link>
-        </li>
+        <router-link to="/" class="navbar-brand nav-link"
+          >Stock Trader</router-link
+        >
+        <router-link
+          to="/portfolio"
+          class="nav-link"
+          activeClass="active"
+          tag="li"
+          ><a>
+            Portfolio
+          </a>
+        </router-link>
+        <router-link
+          to="/stocks"
+          class="nav-link"
+          activeClass="active"
+          tag="li"
+        >
+          <a>
+            Stocks
+          </a>
+        </router-link>
       </ul>
       <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <router-link to="" class="nav-link">End Day</router-link>
+        <router-link to="" class="nav-link" activeClass="active" tag="li">
+          <a>End Day</a>
+        </router-link>
+        <li class="dropdown">
+          <a
+            href="#"
+            class="dropdown-toggle"
+            role="button"
+            aria-haspopup="true"
+            aria-expanded="false"
+            >Save & Load <span class="caret"></span
+          ></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Save Data</a></li>
+            <li><a href="#">Load Data</a></li>
+          </ul>
         </li>
-        <li class="nav-item">
-          <router-link to="" class="nav-link">Save & Load</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="" class="nav-link">Funds: ${{ funds }}</router-link>
-        </li>
+        <router-link to="" class="nav-link" activeClass="active" tag="li">
+          <a>Funds: ${{ funds }}</a>
+        </router-link>
       </ul>
     </nav>
   </div>
---
+++ src/components/portfolio/Stock.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Portfolio Stock</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
